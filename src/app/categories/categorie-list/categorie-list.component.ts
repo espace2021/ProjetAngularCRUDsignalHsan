c71355a51fd41df0633a2e1cb6f28f9f27b8fb11
sh <sandrahammamitlili@gmail.com>
2024-04-28 17:12:35 +0200

    first-commit

--- src/app/categories/categorie-list/categorie-list.component.ts
+++ src/app/categories/categorie-list/categorie-list.component.ts
@@ -7,36 +7,25 @@ import { Categorie } from '../categorie';
 import { CommonModule } from '@angular/common';
 import { ModifcategorieComponent } from "../modifcategorie/modifcategorie.component";
 
-import { DataTablesModule } from "angular-datatables";
-import { Config } from 'datatables.net';
 
 @Component({
     selector: 'app-categorie-list',
     standalone: true,
     templateUrl: './categorie-list.component.html',
     styleUrl: './categorie-list.component.css',
-    imports: [CommonModule, ModifcategorieComponent,DataTablesModule]
+    imports: [CommonModule, ModifcategorieComponent]
 })
 export class CategorieListComponent {
   public categorieService = inject(CategoriesService);
-   
-  dtOptions: Config = {};
-
-   categories = signal<Categorie[]>([]);
+ 
+    categories = signal<Categorie[]>([]);
   ngOnInit(): void {
     this.categories=this.categorieService.getCategories();
-
-    //datatable
-    this.dtOptions = {
-      pagingType: 'full_numbers'
-    };
+   
   }
 
   deleteCategory(category:Categorie):void {
     this.categorieService.deleteCategory(category)
   }
 
-
-  
-  
 }
