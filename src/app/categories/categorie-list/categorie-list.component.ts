28e5bc5c553092310b2fd69c2cf17a6772f3fcd5
sh <sandrahammamitlili@gmail.com>
2024-04-28 18:41:51 +0200

    first-commit

--- src/app/categories/categorie-list/categorie-list.component.ts
+++ src/app/categories/categorie-list/categorie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component,inject, signal  } from '@angular/core';
+import { Component,ViewChild,inject, signal  } from '@angular/core';
 
 
 import { CategoriesService } from '../categories.service';
@@ -7,25 +7,41 @@ import { Categorie } from '../categorie';
 import { CommonModule } from '@angular/common';
 import { ModifcategorieComponent } from "../modifcategorie/modifcategorie.component";
 
+import { MatTableDataSource, MatTableModule } from  '@angular/material/table';
+
+import { MatPaginatorModule } from  '@angular/material/paginator';
+
+import { MatPaginator } from '@angular/material/paginator';
+
 
 @Component({
     selector: 'app-categorie-list',
     standalone: true,
     templateUrl: './categorie-list.component.html',
     styleUrl: './categorie-list.component.css',
-    imports: [CommonModule, ModifcategorieComponent]
+    imports: [CommonModule, ModifcategorieComponent,MatTableModule,MatPaginatorModule,MatPaginator]
 })
 export class CategorieListComponent {
+
   public categorieService = inject(CategoriesService);
+
+  @ViewChild(MatPaginator, { static:  true }) paginator!: MatPaginator;
  
-    categories = signal<Categorie[]>([]);
-  ngOnInit(): void {
-    this.categories=this.categorieService.getCategories();
+  columns: string[] = ['imagecategorie','nomcategorie','_id'];
+
+   categories = signal<Categorie[]>([]);
    
+   categ:any
+    
+ ngOnInit():void {
+  this.categories=this.categorieService.getCategories();
+  
+  this.categ= new MatTableDataSource <any>(this.categories());
+  this.categ.paginator = this.paginator;
   }
 
   deleteCategory(category:Categorie):void {
     this.categorieService.deleteCategory(category)
-  }
+    }
 
 }
