c02e4dc7ec4d7e775129e4e034b330a09a15c270
sh <sandrahammamitlili@gmail.com>
2024-04-29 17:42:00 +0200

    datatable-commit

--- src/app/categories/categorie-list/categorie-list.component.ts
+++ src/app/categories/categorie-list/categorie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild,inject, signal  } from '@angular/core';
+import { Component,inject, signal  } from '@angular/core';
 
 
 import { CategoriesService } from '../categories.service';
@@ -7,41 +7,36 @@ import { Categorie } from '../categorie';
 import { CommonModule } from '@angular/common';
 import { ModifcategorieComponent } from "../modifcategorie/modifcategorie.component";
 
-import { MatTableDataSource, MatTableModule } from  '@angular/material/table';
 
-import { MatPaginatorModule } from  '@angular/material/paginator';
-
-import { MatPaginator } from '@angular/material/paginator';
-
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-categorie-list',
     standalone: true,
     templateUrl: './categorie-list.component.html',
     styleUrl: './categorie-list.component.css',
-    imports: [CommonModule, ModifcategorieComponent,MatTableModule,MatPaginatorModule,MatPaginator]
+    imports: [CommonModule, ModifcategorieComponent,ButtonModule,TableModule]
 })
 export class CategorieListComponent {
+[x: string]: any;
 
   public categorieService = inject(CategoriesService);
 
-  @ViewChild(MatPaginator, { static:  true }) paginator!: MatPaginator;
- 
-  columns: string[] = ['imagecategorie','nomcategorie','_id'];
-
    categories = signal<Categorie[]>([]);
    
-   categ:any
-    
- ngOnInit():void {
+  ngOnInit():void {
   this.categories=this.categorieService.getCategories();
   
-  this.categ= new MatTableDataSource <any>(this.categories());
-  this.categ.paginator = this.paginator;
   }
 
   deleteCategory(category:Categorie):void {
     this.categorieService.deleteCategory(category)
     }
 
+    applyFilterGlobal(dt:any,$event:any, stringVal:any) { 
+      //convertir la cible de l'événement en HTMLInputElement et accéder à la valeur.
+      // filterGlobal fonction prédéfinie de primeng on lui donne la valeur et le type de filtre ici c'est contains envoyé dans stringVal
+      dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+    }
 }
